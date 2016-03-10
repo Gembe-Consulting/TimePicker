dd9df2f62457c035c12ffca2a111c23d94d2708c
PhilippGembe <PhilippGembe@users.noreply.github.com>
2016-03-10 22:09:38 +0100

    Update App.controller.js

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,6 +1,8 @@
 sap.ui.define([
-   "sap/ui/core/mvc/Controller"
-], function (Controller) {
+   "sap/ui/core/mvc/Controller",
+   "sap/ui/model/json/JSONModel",
+   "sap/ui/model/resource/ResourceModel"
+], function (Controller, JSONModel, ResourceModel) {
    "use strict";
    return Controller.extend("de.demo.timepicker.controller.App", {
    });
