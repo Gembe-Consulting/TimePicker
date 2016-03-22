fd516d8b6cc4168192299abc9b316d7ab7d784ab
PhilippGembe <philipp.gembe@gmail.com>
2016-03-22 16:51:34 +0100

    Removed support for locale.
    You should better use binding type of source model.

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -10,7 +10,10 @@ sap.ui.define([
 			var oDataModel = new JSONModel({
 				time1: "223001",
 				time2: "174502",
-				time3: "174503"
+				time3: "174503",
+				time4: "081506",
+				time5: "100101",
+				time6: "175832"
 			});
 			this.getView().setModel(oDataModel,"source");
 
